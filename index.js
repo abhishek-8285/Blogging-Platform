9da6fb29a12ceb19d7a067793eff57fed82639d9
Abhishek Shrivastav <bhshrivastav@gmail.com>
2023-05-02 14:32:00 +0530

    added cors

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const mongoose = require("mongoose");
 const route = require("./src/router");
 const multer = require("multer");
 const errorMiddleware = require("./src/middleware/errorMiddleware");
+const cors = require('cors')
 require("dotenv").config();
+app.use(cors({
+  origin: process.env.CORSURL
+}))
 app.use(
   limitter({
-    windowMs: 15000, // 5 sec,
+    windowMs: 15000, // 15 sec,
     max: 7,
     message: `too many request try again after some time`,
   })
